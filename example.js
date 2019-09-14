d7266e8586634dc84d54927546f8b4a92fe2d108
Julia Friesel <julia.friesel@gmail.com>
2019-09-14 17:18:50 +0000

    Update example.js

--- example.js
+++ example.js
@@ -24,16 +24,16 @@ const cycle = [
 ]
 
 const previousCyle = [
-  { date: '2018-06-01', temperature: 36.7, bleeding: 2 },
-  { date: '2018-06-02', temperature: 36.75 },
-  { date: '2018-06-04', temperature: 36.7 },
-  { date: '2018-06-05', temperature: 36.65 },
-  { date: '2018-06-06', temperature: 36.8, mucus: 0 },
-  { date: '2018-06-13', temperature: 36.9, mucus: 4 },
-  { date: '2018-06-15', temperature: 37.0, mucus: 2 },
-  { date: '2018-06-16', temperature: 37.0, mucus: 2 },
-  { date: '2018-06-17', temperature: 37.0, mucus: 2 },
-  { date: '2018-06-18', temperature: 37.0, mucus: 2 }
+  { date: '2018-05-01', temperature: 36.7, bleeding: 2 },
+  { date: '2018-05-02', temperature: 36.75 },
+  { date: '2018-05-04', temperature: 36.7 },
+  { date: '2018-05-05', temperature: 36.65 },
+  { date: '2018-05-06', temperature: 36.8, mucus: 0 },
+  { date: '2018-05-13', temperature: 36.9, mucus: 4 },
+  { date: '2018-05-15', temperature: 37.0, mucus: 2 },
+  { date: '2018-05-16', temperature: 37.0, mucus: 2 },
+  { date: '2018-05-17', temperature: 37.0, mucus: 2 },
+  { date: '2018-05-18', temperature: 37.0, mucus: 2 }
 ]
 
 console.log(sympto({cycle, previousCyle}))
